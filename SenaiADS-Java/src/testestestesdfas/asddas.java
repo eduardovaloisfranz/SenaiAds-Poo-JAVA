d50e5d5edf68aeb79512c8c714269ee8a65bbf0b
EduardoValoisFranz <38699565+eduardovaloisfranz@users.noreply.github.com>
2019-09-26 21:20:28 -0300

    recursividade

---
+++ SenaiADS-Java/src/testestestesdfas/asddas.java
@@ -0,0 +1,31 @@
+package testestestesdfas;
+
+import java.math.BigDecimal;
+
+public class asddas {
+	public static void main(String[] args) {
+		double x = (0.1 + 0.1 + 0.1) * 3;
+		System.out.println(x);
+
+		BigDecimal a = new BigDecimal("0.1");
+		BigDecimal b = new BigDecimal("0.2");
+		System.out.println(0.1 + 0.2);
+		System.out.println(a.add(b));
+		
+		int total = 0;
+		for(int i = 0; i <= 5; i++) {
+			total += i;
+		}
+		System.out.println(total);
+		
+		System.out.println(soma(1, 5));
+		
+	}
+	public static int soma(int a, int b) {
+		if(a == b) {
+			return a; 
+		}else {
+			return b + soma(a, b-1);
+		}
+	}
+}
