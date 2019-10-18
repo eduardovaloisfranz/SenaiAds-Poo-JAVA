b504f1c487866582bf74dfc8aa3f411455759fbd
outro gato <a@a>
2019-10-17 21:15:07 -0300

    design patters

--- SenaiADS-Java/src/interfaces2/Horista.java
+++ SenaiADS-Java/src/interfaces2/Horista.java
@@ -1,33 +1,35 @@
 package interfaces2;
 
-public class Horista implements ModeloPagamento{
-	private double salarioHora, horasTrabalhadas;	
-	public Horista(double salarioHora, double horasTrabalhadas) {		
+public class Horista implements ModeloPagamento {
+	private double salarioHora;
+	private double horasTrabalhadas;
+
+	public Horista(double salarioHora, double horasTrabalhadas) {
 		this.salarioHora = salarioHora;
 		this.horasTrabalhadas = horasTrabalhadas;
 	}
+
 	@Override
 	public double getSalarioBruto() {
-		if(this.horasTrabalhadas <= 44) {
-			return (this.salarioHora * this.horasTrabalhadas) * 5;	
+		if (this.horasTrabalhadas <= 44) {
+			return (this.salarioHora * this.horasTrabalhadas) * 5;
 		}
-		if(this.horasTrabalhadas > 44) {
-			double salarioBase = (this.salarioHora * 44) * 5; 
-			
+		if (this.horasTrabalhadas > 44) {
+			double salarioBase = (this.salarioHora * 44) * 5;
+
 			double horasAcima = this.horasTrabalhadas - 44;
-			double valorPorHoraExtra = this.salarioHora + (this.salarioHora * 0.5);			
+			double valorPorHoraExtra = this.salarioHora + (this.salarioHora * 0.5);
 			return salarioBase + (horasAcima * valorPorHoraExtra);
-					
-		}		
+
+		}
 		return 0;
-		
+
 	}
+
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		return "E meu salario é: " + getSalarioBruto();
 	}
-	
-	
 
 }
